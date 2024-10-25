f13164e6569cb79f5fe898e51911ebb9493cdd95
fatemarahimi04 <fatema.rahimi004@gmail.com>
2024-10-25 19:43:39 +0200

    unittest

--- Containers/KitchenView/app_kitchen.py
+++ Containers/KitchenView/app_kitchen.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-
+from app_kitchen import app
 app = Flask(__name__)
 
 orders = []
---
+++ Containers/KitchenView/test_kitchenview.py
@@ -0,0 +1,23 @@
+import unittest
+from app_kitchen import app  # Importera kitchen view-appen
+
+class KitchenViewTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_receive_order(self):
+        order_data = {
+            "name": "Hamburgare utan Lök",
+            "price": 89,
+            "ingredients": ["Kött", "Bröd", "Sallad"]
+        }
+        response = self.app.post('/order', json=order_data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_invalid_method(self):
+        response = self.app.get('/order')  # Försök med GET istället för POST
+        self.assertEqual(response.status_code, 405)
+
+if __name__ == '__main__':
+    unittest.main()
